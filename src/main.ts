30449c6dbc3af1713f9961d6b49429bf2a0b767c
Julio <juliovalente321@gmail.com>
2026-03-05 14:58:07 -0300

    feature/create proccess payment use cases

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
+import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
